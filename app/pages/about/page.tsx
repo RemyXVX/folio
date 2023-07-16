2270df027f998bc6d746e88a9a8b9e171c11a3b1
RemyXVX <remyfranciscoflores@gmail.com>
2023-07-15 21:02:47 -0700

    update, projects now are shown and are link to github repo!

--- app/pages/about/page.tsx
+++ app/pages/about/page.tsx
@@ -14,7 +14,7 @@ const About = () => {
           <div>
             <div>
               <h2 className="text-gray-700 text-lg sm:text-xl md:text-2xl font-medium mb-1">
-                My name is Remy! I'am a Full Stack Developer
+                My name is Remy and I'm a Web Developer
               </h2>
               <p className="text-gray-700 text-base sm:text-lg md:text-xl font-light leading-relaxed mb-1">
                 Allow me to introduce myself as a full-stack developer extraordinaire! With an unwavering passion for crafting innovative solutions, I possess an innate ability to build projects from the ground up. Seamlessly blending front-end wizardry with cutting-edge and interactive UI, I orchestrate a symphony of features driven by my back-end prowess. Equipped with a versatile skill set encompassing ReactJS, NodeJS, and Asp.Net Core, I breathe life into dynamic applications, crafting digital marvels with utmost finesse. Join me on an exhilarating journey where innovation and functionality harmoniously intertwine!
--- app/pages/projects/page.tsx
+++ app/pages/projects/page.tsx
@@ -1,45 +1,66 @@
-import React from 'react';
-import { useSwipeable } from 'react-swipeable';
+import React, { useEffect, useState } from 'react';
 import Pizzapalo from './pizzapalo';
 import Distro from './distro';
 
 const Projects = () => {
-  const images = [
-    'https://example.com/image-url-1.jpg',
-    'https://example.com/image-url-2.jpg',
-    'https://example.com/image-url-3.jpg',
-  ];
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-  const [currentImageIndex, setCurrentImageIndex] = React.useState(0);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 768);
+    };
 
-  const prevImage = () => {
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    );
-  };
+    handleResize(); // Set initial screen size
+    window.addEventListener('resize', handleResize); // Listen for resize events
 
-  const nextImage = () => {
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === images.length - 1 ? 0 : prevIndex + 1
-    );
-  };
-
-  const handlers = useSwipeable({
-    onSwipedLeft: nextImage,
-    onSwipedRight: prevImage,
-  });
+    return () => {
+      window.removeEventListener('resize', handleResize); // Clean up the event listener
+    };
+  }, []);
 
   return (
-    <div className="grid grid-cols-2 gap-4">
-      <div>
-        <Distro />
-        <p className="text-xl">Distro Project Description</p>
+    <>
+      <div className="text-left" id="projects" />
+      <div className="pt-4">
+        <h1 className="text-4xl font-bold pb-4">Projects:</h1>
       </div>
-      <div>
-        <Pizzapalo />
-        <p className="text-xl">Pizzapalo Project Description</p>
+      <div className="pt-4">
+        <div className="grid grid-cols-2 grid-rows-2 gap-4">
+          <div className="flex justify-center items-center">
+            <Pizzapalo />
+          </div>
+          <div className="pt-4">
+            <h1 className="text-xl font-bold font-mono py-4">Pizzapalo</h1>
+            <div className={`${isSmallScreen ? 'h-[150px] overflow-auto' : ''} text-center`}>
+              <p className="text-gray-700">
+                Pizzapalo is a responsive web application designed for pizza shops, offering users a seamless pizza customization experience. Built with Vite, React, and Tailwind CSS, this project showcases the power of objects, for loops, and other methods to create a dynamic and user-friendly interface. Whether it's choosing toppings or customizing crust options, Pizzapalo provides a flexible and adaptable solution to enhance any pizza shop's online ordering process.
+              </p>
+            </div>
+            <div className="mt-4">
+              <a href="https://github.com/RemyXVX/pizzashop" className="text-rose-200 font-bold bg-slate-400 hover:bg-slate-300 py-2 px-4 rounded-md">
+                Click me
+              </a>
+            </div>
+          </div>
+          <div className="flex justify-center items-center">
+            <Distro />
+          </div>
+          <div className="pb-4">
+            <h1 className="text-xl font-bold font-mono py-4">Coffee Distro</h1>
+            <div className={`${isSmallScreen ? 'h-[150px] overflow-auto' : ''} text-center`}>
+              <p className="text-gray-700">
+                I am developing a full e-commerce web application using VSCode, JavaScript, Chakra UI, Tailwind, JSX, Node, React, Babel, and ESLint for the Doomed Coffee Shop. The website enables users to conveniently order their favorite coffee and treats online. The app includes a seamless ordering system where users can place orders, and with each sale, the quantity of coffee automatically decreases by 1. Additionally, I am implementing a comprehensive e-commerce solution, allowing users to create their own websites, place orders, and set up automatic order notifications when the coffee quantity runs low.
+              </p>
+            </div>
+            <div className="mt-4">
+              <a href="https://github.com/RemyXVX/Doomed-Coffe-Shop" className="text-rose-200 font-bold bg-slate-400 hover:bg-slate-300 py-2 px-4 rounded-md">
+                Click me
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 

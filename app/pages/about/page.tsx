5cf0cfa202d52bdfc897a18fc4b06283275576b3
RemyXVX <remyfranciscoflores@gmail.com>
2023-07-15 21:21:54 -0700

    like a broke record, deply

--- app/pages/about/page.tsx
+++ app/pages/about/page.tsx
@@ -17,7 +17,7 @@ const About = () => {
                 My name is Remy and I'm a Web Developer
               </h2>
               <p className="text-gray-700 text-base sm:text-lg md:text-xl font-light leading-relaxed mb-1">
-                Allow me to introduce myself as a full-stack developer extraordinaire! With an unwavering passion for crafting innovative solutions, I possess an innate ability to build projects from the ground up. Seamlessly blending front-end wizardry with cutting-edge and interactive UI, I orchestrate a symphony of features driven by my back-end prowess. Equipped with a versatile skill set encompassing ReactJS, NodeJS, and Asp.Net Core, I breathe life into dynamic applications, crafting digital marvels with utmost finesse. Join me on an exhilarating journey where innovation and functionality harmoniously intertwine!
+                Allow me to introduce myself as a developer extraordinaire! With an unwavering passion for crafting innovative solutions, I possess an innate ability to build projects from the ground up. Seamlessly blending front-end wizardry with cutting-edge and interactive UI, I orchestrate a symphony of features driven by my back-end prowess. Equipped with a versatile skill set encompassing ReactJS, NodeJS, and Asp.Net Core, I breathe life into dynamic applications, crafting digital marvels with utmost finesse. Join me on an exhilarating journey where innovation and functionality harmoniously intertwine!
               </p>
             </div>
             <div className="pt-4">
